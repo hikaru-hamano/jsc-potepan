3b622c3232a488da8a929156844865ec58ede2cc
EC2 Default User <ec2-user@ip-172-31-35-198.us-east-2.compute.internal>
2021-09-18 08:01:21 +0000

    再提出

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ let milliseconds = 0;
 let status = "stop";
 let interval;
 
+
+
 function stopWatch(){
   
  milliseconds++;
@@ -31,13 +33,19 @@ function stopWatch(){
   
   
 display.innerHTML = hours + ":" + minutes + ":" + seconds + ":" + milliseconds;
-}
 
+}
 
 start.addEventListener("click", function(){
    interval = setInterval(stopWatch, 100);
     start.innerHTML = "スタート";
     status = "start";
+   start.classList.add('inactive');  // 非活性
+    stop.classList.remove('inactive');  // 活性
+    reset.classList.add('inactive');   // 非活性
+    
+    document.getElementById("start").disabled = "true";
+    document.getElementById("reset").disabled = "true";
 });
 
 if(status == "stop"){
@@ -45,6 +53,12 @@ stop.addEventListener("click", function(){
   clearInterval(interval);
   stop.innerHTML = "ストップ";
   status = "stop";
+  start.classList.remove('inactive'); // 活性
+    stop.classList.add('inactive');    // 非活性
+    reset.classList.remove('inactive'); // 活性
+
+    document.getElementById("start").disabled = "";   
+    document.getElementById("reset").disabled = "";   
 
 });
 }
@@ -53,10 +67,17 @@ reset.addEventListener("click",function() {
      clearInterval(interval);
      stop.innerHTML = "ストップ";
   status = "stop";
+  
+  start.classList.remove('inactive'); // 活性
+    stop.classList.add('inactive');   // 非活性
+    reset.classList.add('inactive');   // 非活性
+    
 display.innerHTML = "0:0:0:0";
 hours = 0;
 minutes = 0;
 seconds = 0;
 milliseconds = 0;
+
+
 });
 
